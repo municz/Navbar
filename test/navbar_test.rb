593525d858a9dfe0c92011bd230646153a8bdc4b
Ruby <ruby@ruby-muni.(none)>
2011-09-21 12:27:35 +0200

    Test

---
+++ test/navbar_test.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+
+class NavbarTest < Test::Unit::TestCase
+  def setup
+    @navbar = Navbar.new
+    user = Navbar.new("User","/user")
+    @navbar.add_child(user)
+    user_show = Navbar.new("Show","/user")
+    user.add_child(user_show)
+    user_edit = Navbar.new("Edit","/user/edit")
+    user.add_child(user_edit)
+  end
+
+  def test_html_output
+    assert_equal(<<EOS, @navbar.html_output)
+<ul>
+  <li><a href="/user">User</a>
+    <ul>
+      <li><a href="/user">Show</a></li>
+      <li><a href="/user/edit">Edit</a></li>
+    </ul>
+  </li>
+</ul>
+EOS
+  end
+
+  def test_xml_output
+    assert_equal(<<EOS, @navbar.xml_output)
+<navbar>
+  <item name="User" href="/user">
+    <item name="Show" href="/user"/>
+    <item name="Edit" href="/user/edit"/>
+  </item>
+</navbar>
+EOS
+  end
+end
