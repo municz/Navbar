b4e6a2a9a2ec98d96a933d3b819dcfd9f20012f2
Ivan Necas <inecas@redhat.com>
2011-09-25 17:04:32 +0200

    Refactor xml_output

--- lib/navbar.rb
+++ lib/navbar.rb
@@ -1,14 +1,14 @@
 require 'erb'
 
 class Navbar
-  attr_writer :html_template_path
+  attr_writer :html_template_path, :xml_template_path
   attr_accessor :parent
   attr_reader :name, :path
   include Enumerable
 
   def initialize(name = nil, path = nil)
     @name, @path = name, path
-    @children, @html_template_path = [], nil
+    @children, @html_template_path, @xml_template_path = [], nil, nil
   end
 
   def each(&block)
@@ -18,6 +18,10 @@ class Navbar
 
   def html_template_path
     @html_template_path || parent.html_template_path
+  end
+
+  def xml_template_path
+    @xml_template_path || parent.xml_template_path
   end
 
   def add_child(child)
@@ -31,21 +35,8 @@ class Navbar
   end
 
   def xml_output
-    output = ""
-    output << unless @name
-      "<navbar>"
-    else
-      %{<item name="#{@name}" href="#{@path}"#{@children.empty? ? "/" : ""}>}
-    end
-    @children.each do |child|
-      output << child.xml_output
-    end
-    output << unless @name
-      "</navbar>"
-    else
-      @children.empty? and "" or "</item>" 
-    end
-    output
+    template = ERB.new(File.read(xml_template_path))
+    template.result(self.send :binding)
   end
 
   def path_to_name(path)
